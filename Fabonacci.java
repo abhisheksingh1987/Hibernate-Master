f7fd018611bf30cb89b0f27f6df1bfe19c5fc004
Abhishek <abhisheksinghk737@gmail.com>
2021-10-29 11:01:56 +0530

    first commit

---
+++ Fabonacci.java
@@ -0,0 +1,15 @@
+package com.abs;
+
+public class Fabonacci {
+	public static void main(String[] args) {
+		int j=0,k=1;
+		System.out.print(j+","+k+",");
+		for(int i=2;i<=30;i++) {
+		int c=j+k;
+		System.out.print(c+",");
+		j=k;
+		k=c;
+		}
+	
+	}
+	}
